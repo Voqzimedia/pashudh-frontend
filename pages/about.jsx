5ab20f92b7dafc551467dc8a67727969ffed576e
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2021-08-18 18:33:29 +0530

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -89,38 +89,67 @@ export default function About() {
       <section className={`our-silk-route-section page-section`}>
         <Container>
           <center>
-            <h2 className="section-title center">Our Silk Route</h2>
+            <h2 className="section-title center">
+              Silks that sing tales of heritage, legacy, and excellence.
+            </h2>
           </center>
           <article className="content-holder">
             <p>
-              Pashudh was born out of a passion to celebrate the glorious silk
-              traditions of our land and embrace contemporary trends. Pure,
-              authentic Kanjeevaram that has been treasured by generations of
-              women needs to make its way into the wardrobes of new-age women
-              who follow haute couture as well.
+              The journey that started with the vision of becoming the pioneer
+              in silks has now reached the destination of being a name
+              synonymous with eternal style. Pashudh's story is not just that of
+              a brand; it's the history of a silk-weaving community.
             </p>
             <p>
-              Our journey began 40 years ago in Arni, a town in Tamil Nadu that
-              is renowned for its rich legacy of silk sarees. Pashudh was
-              founded by the brothers - KK Rajkumar, KK Sureshkumar and KV Kumar
-              whose entrepreneurial skills and fascination for silk made Pashudh
-              a name to reckon with.
+              A visionary who embarked on his life's voyage as a weaver, Kanchi
+              K Ethirajulu, made the transition to being a designer in 1950.
+              Subsequently, he started handling multiple weaving looms, and the
+              family business flourished with each new generation. Introducing
+              the mechanical jacquard and many such innovations in the field of
+              handlooms enabled us to gain a stronghold in the business world.
+              Our foundation was K. V. Kumar Silks, headed by K. K. Rajkumar and
+              K.K. Suresh Kumar, with a focus on B2B sales, wherein the
+              world-renowned Kanjivaram silk sarees created were sold directly
+              to retailers. The expertise of our weavers and solid
+              infrastructure comprising world-class machinery in weaving
+              technology enabled us to produce top-quality Kanjivaram silk
+              sarees.
             </p>
             <p>
-              Mr KV Kumar has proven his business acumen by establishing key
-              partnerships with retailers all over the country. He is also an
-              illustrious designer whose creations have flown off the shelves.
-              By keeping abreast of new developments in technology, trends and
-              customer demands, he has been the driving force behind the brand’s
-              success.
+              Ms Jeevitha Rajkumar, our CEO and Ms Vinodhini Suresh, our CMD,
+              wanted to enter the B2C market, which gave birth to Pashudh.
+              Established in Arani, a quaint little town in the Thiruvannamalai
+              district, Tamil Nadu, Pashudh prides itself on creating a strong
+              digital presence, delivering magnificent Kanjivaram silk sarees
+              globally.
             </p>
             <p>
-              Our story is also that of the revival of weaving communities. We
-              have an award-winning team of 750 weavers, many of whom belonging
-              to families that have been in the trade for generations. We
-              combine conventional methods of weaving with more recent
-              techniques as well. All our sarees are lovingly woven by hand, and
-              proudly draped by lovely women all over the world.
+              Upholding a design philosophy that adapts heritage Kanjivaram silk
+              weaves to suit the sensibilities of modern women, Pashudh offers a
+              wide variety of silk sarees that you can flaunt on all kinds of
+              occasions, right from casual to elite. Lending the love of the
+              craft a personal touch, our Managing Directors themselves
+              contribute their insights while designing the sarees. This results
+              in each Pashudh saree exemplifying the virtues of contemporary
+              design, ultimate precision, exceptional quality, and optimum
+              comfort.
+            </p>
+            <p>
+              With great legacy comes great responsibility. The weavers'
+              community that forms the backbone of Pashudh's path to success
+              comprises about 750 people, all of whom live and breathe silk
+              sarees. The organization is focused on equipping them with
+              technological advancements, education, and anything else necessary
+              to aid in their evolution into a self-sustaining and thriving
+              community of weavers.
+            </p>
+            <p>
+              Pashudh aspires to present the magic of our looms before the
+              world, with a level of finesse and elegance that has never been
+              seen before. Furthermore, we wish to communicate our love for
+              weaving silk sarees and our legacy through our products. One day,
+              we hope the global community would think Pashudh every time they
+              think of silk sarees.
             </p>
           </article>
         </Container>
