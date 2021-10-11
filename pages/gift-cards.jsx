aba37d9ae7b3ef03d6fd1f286a3b1a1bc39b4e1a
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2021-10-11 15:27:28 +0530

    Accordion added in gift card page

--- pages/gift-cards.jsx
+++ pages/gift-cards.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useMemo } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Link from "next/link";
 
@@ -14,6 +14,7 @@ import dynamic from "next/dynamic";
 import SvgIcon from "../components/utils/SvgIcon";
 
 import giftCardImg from "../assets/images/giftCard.png";
+import Accordion, { AccordionItem } from "../components/utils/accordion";
 
 const PageMotion = dynamic(() => import("../components/Motion/PageMotion"));
 const Logo = dynamic(() => import("../components/Logo"));
@@ -35,13 +36,13 @@ export default function GiftCards() {
 
   // console.log(giftCard);
 
+  useMemo(() => {
+    addGiftCard(selectedGiftCard);
+  }, [selectedGiftCard]);
+
   const onGiftCardSelect = (event) => {
-    // console.log(event.target.value);
     setIsLoading(true);
-
     setSelectedGiftCard(event.target.value);
-    addGiftCard(event.target.value);
-
     setIsLoading(false);
   };
 
@@ -140,91 +141,102 @@ export default function GiftCards() {
                   </p>
                 </div>
                 <br />
-                <div className="content-holder">
-                  {/* <h2 className="section-title content-title">How to use -</h2>
-                  <br /> */}
-                  <h4 className="section-title content-title">
-                    Ordering Pashudh Gift Cards
-                  </h4>
-                  <p>
-                    Go to our Pashudh Gift Cards page, select your preferred
-                    Gift Card denomination, type the e-mail id of the recipient
-                    you want to gift the card to, and add it to your cart.
-                  </p>
-                  <p>Once you have finished proceed to the checkout page.</p>
-                  <p>
-                    On our checkout page, make sure you cross-check the
-                    denomination and e-mail id of the recipient before
-                    proceeding to payment.
-                  </p>
-                </div>
-                <br />
-                <div className="content-holder ">
-                  <h4 className="section-title content-title">
-                    Redeeming the Pashudh Gift Cards
-                  </h4>
-                  <p>
-                    A surprise awaits in your mailbox! You’ve received an e-mail
-                    from Pashudh and you open it to see that your loved one has
-                    sent you a Pashudh Gift Card!
-                  </p>
-                  <p>
-                    Click on the ‘Copy’ button displayed below the Pashudh Gift
-                    Cards in your e-mail. This will ensure that the code with
-                    the intended denomination is copied to your device and can
-                    further be redeemed on our website.
-                  </p>
-                  <p>
-                    In case you had opted to send the Pashudh Gift Cards to your
-                    own e-mail id, and then wish to forward the code to your
-                    loved ones, the same process can be followed. Kindly ensure
-                    that you share the code with the right person because
-                    Pashudh will not be responsible for any such errors.
-                  </p>
-                  <p>
-                    Once you copy the code, head to our website, shop to your
-                    heart’s content, and once you are over the moon with the
-                    products in your cart, paste the code in the “Gift Card
-                    Code” box at checkout.
-                  </p>
-                  <p>
-                    Voila! The value of the Gift Card will be assigned to your
-                    checkout amount.
-                  </p>
-                </div>
-                <br />
-                <div className="content-holder end-stick">
-                  <h4 className="section-title content-title">
-                    Terms & Conditions
-                  </h4>
-                  <p>
-                    A few points to note when opting for Pashudh Gift Cards -
-                  </p>
-                  <p>
-                    The Pashudh Gift Cards are paperless and viable only for
-                    online transactions through our website. There will be no
-                    processing charges while redeeming the value of the Pashudh
-                    Gift Cards. Those receiving our e-mail containing the Gift
-                    Cards can redeem it online, but not in any physical stores
-                    or exhibitions (in the future).
-                  </p>
-                  <p>
-                    Pashudh Gift Cards, once allocated, cannot be transferred to
-                    anyone else.
-                  </p>
-                  <p>
-                    Please ensure that correct details are entered while
-                    purchasing and delegating the Pashudh Gift Cards.
-                    Cross-check the amount, and e-mail id of the recipient/s
-                    before the final checkout.
-                  </p>
-                  <p>
-                    The validity of our Gift Cards will last for a period of 1
-                    year from the date of issue.
-                  </p>
-                  <p>
-                    The value of the Gift Cards cannot be exchanged for cash.
-                  </p>
+                <div className="gift-content-holder">
+                  <Accordion atomic={true}>
+                    <AccordionItem title={`Ordering Pashudh Gift Cards`}>
+                      <div className="gift-content">
+                        <article className="content-holder">
+                          <p>
+                            Go to our Pashudh Gift Cards page, select your
+                            preferred Gift Card denomination, type the e-mail id
+                            of the recipient you want to gift the card to, and
+                            add it to your cart.
+                          </p>
+                          <p>
+                            Once you have finished proceed to the checkout page.
+                          </p>
+                          <p>
+                            On our checkout page, make sure you cross-check the
+                            denomination and e-mail id of the recipient before
+                            proceeding to payment.
+                          </p>
+                        </article>
+                      </div>
+                    </AccordionItem>
+                    <AccordionItem title={`Redeeming the Pashudh Gift Cards`}>
+                      <div className="support-item">
+                        <article className="support-content">
+                          <p>
+                            A surprise awaits in your mailbox! You’ve received
+                            an e-mail from Pashudh and you open it to see that
+                            your loved one has sent you a Pashudh Gift Card!
+                          </p>
+                          <p>
+                            Click on the ‘Copy’ button displayed below the
+                            Pashudh Gift Cards in your e-mail. This will ensure
+                            that the code with the intended denomination is
+                            copied to your device and can further be redeemed on
+                            our website.
+                          </p>
+                          <p>
+                            In case you had opted to send the Pashudh Gift Cards
+                            to your own e-mail id, and then wish to forward the
+                            code to your loved ones, the same process can be
+                            followed. Kindly ensure that you share the code with
+                            the right person because Pashudh will not be
+                            responsible for any such errors.
+                          </p>
+                          <p>
+                            Once you copy the code, head to our website, shop to
+                            your heart’s content, and once you are over the moon
+                            with the products in your cart, paste the code in
+                            the “Gift Card Code” box at checkout.
+                          </p>
+                          <p>
+                            Voila! The value of the Gift Card will be assigned
+                            to your checkout amount.
+                          </p>
+                        </article>
+                      </div>
+                    </AccordionItem>
+                    <AccordionItem title={`Terms & Conditions`}>
+                      <div className="gift-content">
+                        <article className="content-holder">
+                          <p>
+                            A few points to note when opting for Pashudh Gift
+                            Cards -
+                          </p>
+                          <p>
+                            The Pashudh Gift Cards are paperless and viable only
+                            for online transactions through our website. There
+                            will be no processing charges while redeeming the
+                            value of the Pashudh Gift Cards. Those receiving our
+                            e-mail containing the Gift Cards can redeem it
+                            online, but not in any physical stores or
+                            exhibitions (in the future).
+                          </p>
+                          <p>
+                            Pashudh Gift Cards, once allocated, cannot be
+                            transferred to anyone else.
+                          </p>
+                          <p>
+                            Please ensure that correct details are entered while
+                            purchasing and delegating the Pashudh Gift Cards.
+                            Cross-check the amount, and e-mail id of the
+                            recipient/s before the final checkout.
+                          </p>
+                          <p>
+                            The validity of our Gift Cards will last for a
+                            period of 1 year from the date of issue.
+                          </p>
+                          <p>
+                            The value of the Gift Cards cannot be exchanged for
+                            cash.
+                          </p>
+                        </article>
+                      </div>
+                    </AccordionItem>
+                  </Accordion>
                 </div>
               </div>
             </Col>
